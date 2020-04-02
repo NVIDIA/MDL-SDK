9bd0cf7a98af7e9d050b0d2be2a24c7e6a75245d
Lutz Kettner <42216493+lkettnerNV@users.noreply.github.com>
2020-04-02 13:58:37 +0200

    Update copyright year to 2020

--- src/render/mdl/runtime/mdlrt_bsdf_measurement.cpp
+++ src/render/mdl/runtime/mdlrt_bsdf_measurement.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2014-2019, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2014-2020, NVIDIA CORPORATION. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
