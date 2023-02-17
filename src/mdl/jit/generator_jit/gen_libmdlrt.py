5325d75553eda6d12ee32c3772c95afe650674b0
Moritz Haenke de Cansino <29729967+mkroll-nv@users.noreply.github.com>
2023-02-17 13:03:34 +0100

    Update copyright year to 2023

--- src/mdl/jit/generator_jit/gen_libmdlrt.py
+++ src/mdl/jit/generator_jit/gen_libmdlrt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #*****************************************************************************
-# Copyright (c) 2019-2022, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2019-2023, NVIDIA CORPORATION. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
